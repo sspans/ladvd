f52aa448958f95557c310b2a050c45acdd59634f
sten <devnull@localhost>
2008-05-15 20:35:59 +0000

    lowercase strings add wip support for bsd siogifmedia ioctl

--- src/ifinfo.c
+++ src/ifinfo.c
@@ -16,6 +16,10 @@
 # include <linux/ethtool.h>
 #endif /* HAVE_LINUX_ETHTOOL_H */
 
+#if HAVE_NET_IF_MEDIA_H
+# include <net/if_media.h>
+#endif /* HAVE_NET_IF_MEDIA_H */
+
 int ifinfo_get(struct session *session) {
     int fd;
     struct ifreq ifr;
@@ -24,17 +28,21 @@ int ifinfo_get(struct session *session) {
     struct ethtool_cmd ecmd;
 #endif /* HAVE_LINUX_ETHTOOL_H */
 
+#if HAVE_NET_IF_MEDIA_H
+    struct ifmediareq ifmr;
+#endif /* HAVE_HAVE_NET_IF_MEDIA_H */
+
     fd = libnet_getfd(session->libnet);
     session->duplex = -1;
     session->autoneg_supported = -1;
     session->autoneg_enabled = -1;
 
-    // fetch interface mtu
-    (void) memset(&ifr, 0, sizeof(ifr));
-    (void) strncpy(ifr.ifr_name, session->dev, sizeof(ifr.ifr_name) -1);
+    bzero(&ifr, sizeof(ifr));
+    strncpy(ifr.ifr_name, session->dev, sizeof(ifr.ifr_name) -1);
 
+    // interface mtu
     if (ioctl(fd, SIOCGIFMTU, (caddr_t)&ifr) < 0) {
-	log_str(0, "Fetching %s mtu failed: %s", session->dev, strerror(errno));
+	log_str(0, "fetching %s mtu failed: %s", session->dev, strerror(errno));
     } else {
 	session->mtu = ifr.ifr_mtu;
     }
@@ -47,6 +55,7 @@ int ifinfo_get(struct session *session) {
 	session->duplex = (ecmd.duplex == DUPLEX_FULL) ? 1 : 0;
 	session->speed = ecmd.speed;
 
+	// autoneg
 	if(ecmd.supported & SUPPORTED_Autoneg) {
 	    session->autoneg_supported = 1;
 	    session->autoneg_enabled = (ecmd.autoneg == AUTONEG_ENABLE) ? 1 : 0;
@@ -54,7 +63,55 @@ int ifinfo_get(struct session *session) {
 	    session->autoneg_supported = 0;
 	}	
     }
+
+    return(EXIT_SUCCESS);
 #endif /* HAVE_LINUX_ETHTOOL_H */
 
-    return (EXIT_SUCCESS);
+#if HAVE_NET_IF_MEDIA_H
+    bzero(&ifmr, sizeof(ifmr));
+    strncpy(ifmr.ifm_name, session->dev, sizeof(ifmr.ifm_name) -1);
+
+    if (ioctl(s, SIOCGIFMEDIA, (caddr_t)&ifmr) < 0) {
+	// media detection not supported
+	return(EXIT_SUCCESS);
+    }
+
+    if (ifmr.ifm_count == 0) {
+	log_str(0, "missing media types for interface %s", session->dev);
+	return(EXIT_FAILURE);
+    }
+
+    ifmr.ifm_ulist = malloc(ifmr.ifm_count * sizeof(int));
+    if (ifmr.ifm_ulist == NULL) {
+	log_str(0, "malloc failed for interface %s", session->dev);
+	return(EXIT_FAILURE);
+    }
+
+    if (ioctl(s, SIOCGIFMEDIA, (caddr_t)&ifmr) < 0) {
+	log_str(0, "media detection failed for interface %s", session->dev);
+	return(EXIT_FAILURE);
+    }
+
+    if (IFM_TYPE(ifmr.ifm_current) != IFM_ETHER) {
+	log_str(0, "non-ethernet interface %s found", session->dev);
+	return(EXIT_FAILURE);
+    }
+
+    // autoneg
+    if(ifmr.ifm_current & IFM_AUTO) {
+	session->autoneg_supported = 1;
+	session->autoneg_enabled = 1;
+    } else {
+	session->autoneg_supported = 1;
+	session->autoneg_enabled = 0;
+    }
+
+    if(ifmr.ifm_active & IFM_FDX) {
+	session->autoneg_supported = 1;
+    }
+
+    return(EXIT_SUCCESS);
+#endif /* HAVE_NET_IF_MEDIA_H */
+
+    return(EXIT_SUCCESS);
 }
