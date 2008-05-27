efd5b96839bdb33a7d11790f3e3d219bdad06c54
sten <devnull@localhost>
2008-05-27 11:45:22 +0000

    push additional null-byte into location, seems to be needed

--- src/cdp.c
+++ src/cdp.c
@@ -124,6 +124,7 @@ size_t cdp_encode(struct cdp_packet *packet, void *data, size_t length) {
 	END_CDP_TLV;
 
 	if (packet->location != NULL && !(
+	    PUSH_UINT8(0) &&
 	    START_CDP_TLV(CDP_TYPE_LOCATION) &&
 	    PUSH_BYTES(packet->location, strlen(packet->location))
 	))
