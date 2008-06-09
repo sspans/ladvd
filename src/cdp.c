000c98abcbaf1b167f71de70a9f255a447b450a9
sten <devnull@localhost>
2008-06-09 22:25:56 +0000

    update to reflect main/util changes

--- src/cdp.c
+++ src/cdp.c
@@ -67,7 +67,7 @@ size_t cdp_encode(struct cdp_packet *packet, void *data, size_t length) {
 		PUSH_BYTES(cdp_predefs[CDP_ADDR_PROTO_IPV4].protocol,
 			   cdp_predefs[CDP_ADDR_PROTO_IPV4].protocol_length) &&
 		PUSH_UINT16(sizeof(packet->address4)) &&
-		PUSH_UINT32(packet->address4)
+		PUSH(packet->address4, uint32_t,)
 	    ))
 		return 0;
 
@@ -138,43 +138,44 @@ size_t cdp_encode(struct cdp_packet *packet, void *data, size_t length) {
 	return VOIDP_DIFF(pos, data);
 }
 
-int cdp_packet(struct session *session) {
-    struct cdp_packet *packet;
+int cdp_packet(struct session *csession, struct session *session,
+	       struct sysinfo *sysinfo) {
 
-    packet = malloc(sizeof(struct cdp_packet));
-    bzero(packet, sizeof(struct cdp_packet));
+    struct cdp_packet packet;
 
-    packet->version = 2;
-    packet->ttl = 180;
-    packet->device_id = session->hostname;
-    packet->ios_version = session->uts_str;
-    packet->platform = session->uts->sysname;
-    packet->port_id = session->dev;
-    packet->mtu = session->mtu;
-    packet->system_name = session->hostname;
-    packet->location = session->location;
+    packet.version = 2;
+    packet.ttl = LADVD_TTL;
+    packet.device_id = sysinfo->hostname;
+    packet.ios_version = sysinfo->uts_str;
+    packet.platform = sysinfo->uts.sysname;
+    packet.port_id = csession->if_name;
+    packet.mtu = csession->mtu;
+    packet.system_name = sysinfo->hostname;
+    packet.location = sysinfo->location;
 
-    if (session->cap & CAP_BRIDGE)
-    	packet->capabilities |= CDP_CAP_TRANSPARENT_BRIDGE;
-    if (session->cap & CAP_HOST)
-    	packet->capabilities |= CDP_CAP_HOST;
-    if (session->cap & CAP_ROUTER)
-    	packet->capabilities |= CDP_CAP_ROUTER;
-    if (session->cap & CAP_SWITCH)
-    	packet->capabilities |= CDP_CAP_SWITCH;
+    if (sysinfo->cap & CAP_BRIDGE)
+    	packet.capabilities |= CDP_CAP_TRANSPARENT_BRIDGE;
+    if (sysinfo->cap & CAP_HOST)
+    	packet.capabilities |= CDP_CAP_HOST;
+    if (sysinfo->cap & CAP_ROUTER)
+    	packet.capabilities |= CDP_CAP_ROUTER;
+    if (sysinfo->cap & CAP_SWITCH)
+    	packet.capabilities |= CDP_CAP_SWITCH;
 
-    if (session->ipaddr4 != -1)
-    	packet->address4 = session->ipaddr4;
+    // parent interface
+    if (session->ipaddr4 != 0)
+    	packet.address4 = session->ipaddr4;
 
-    if (session->duplex != -1)
-    	packet->duplex = session->duplex;
 
-    session->cdp_data = malloc(BUFSIZ);
-    bzero(session->cdp_data, BUFSIZ);
+    // media information
+    if (csession->duplex != -1)
+    	packet.duplex = csession->duplex;
 
-    session->cdp_length = cdp_encode(packet, session->cdp_data, BUFSIZ);
-    if (session->cdp_length == 0) {
-	log_str(0, "generated cdp packet too large");
+    bzero(csession->cdp_data, BUFSIZ);
+
+    csession->cdp_length = cdp_encode(&packet, csession->cdp_data, BUFSIZ);
+    if (csession->cdp_length == 0) {
+	my_log(0, "generated cdp packet too large");
 	return(EXIT_FAILURE);
     }
 
@@ -183,34 +184,34 @@ int cdp_packet(struct session *session) {
 
 int cdp_send(struct session *session) {
 
-    libnet_t *l = session->libnet;
+   libnet_t *l = session->libnet;
 
-    if (libnet_build_data(session->cdp_data, session->cdp_length, l, 0) == -1) {
-        log_str(0, "can't build cdp ethernet data: %s", libnet_geterror(l));
-        goto fail;
-    }
+   if (libnet_build_data(session->cdp_data, session->cdp_length, l, 0) == -1) {
+	my_log(0, "can't build cdp ethernet data: %s", libnet_geterror(l));
+	goto fail;
+   }
 
-    if (libnet_build_802_2snap(0xaa, 0xaa, 0x03, cdp_snap_oui, 
-			       0x2000, NULL, 0, l, 0) == -1) {
-        log_str(0, "can't build cdp snap header: %s", libnet_geterror(l));
+   if (libnet_build_802_2snap(0xaa, 0xaa, 0x03, cdp_snap_oui, 
+			      0x2000, NULL, 0, l, 0) == -1) {
+	my_log(0, "can't build cdp snap header: %s", libnet_geterror(l));
+	goto fail;
+   }
+
+   /* length is 802.2 SNAP header + CDP's length */
+   if(libnet_build_802_3(cdp_mac, libnet_get_hwaddr(l),
+			 LIBNET_802_2SNAP_H + session->cdp_length,
+			 NULL, 0, l, 0) == -1) {
+
+	my_log(0, "can't build cdp ethernet header: %s", libnet_geterror(l));
 	goto fail;
     }
 
-    /* length is 802.2 SNAP header + CDP's length */
-    if(libnet_build_802_3(cdp_mac, session->hwaddr,
-			LIBNET_802_2SNAP_H + session->cdp_length,
-			NULL, 0, l, 0) == -1) {
-
-	log_str(0, "can't build cdp ethernet header: %s", libnet_geterror(l));
+   /*
+    *  Write it to the wire.
+    */
+   if (libnet_write(l) == -1) {
+	my_log(0, "network transmit error: %s", libnet_geterror(l));
 	goto fail;
-    }
-
-    /*
-     *  Write it to the wire.
-     */
-    if (libnet_write(l) == -1) {
-        log_str(0, "network transmit error: %s", libnet_geterror(l));
-        goto fail;
     }
 
     libnet_clear_packet(l);
