a6f2d92394524a2c5f992bf600c7e349c29b05e4
sten <devnull@localhost>
2008-07-20 23:54:38 +0000

    rewrite tlv.h using memcpy to it work on strict alignment archs like sparc64

--- src/tlv.h
+++ src/tlv.h
@@ -4,44 +4,53 @@
 #define VOIDP_DIFF(P, Q) ((uintptr_t)((char *)(P) - (char *)(Q)))
 #define VOIDP_OFFSET(P, O) ((void *)((char *)(P) + (uintptr_t)(O)))
 
+union {
+    uint8_t uint8;
+    uint16_t uint16;
+    uint32_t uint32;
+} types;
 
 #define PUSH(value, type, func) \
 	((length >= sizeof(type)) && \
-		( \
-			*((type*)pos) = func(value), \
-			length -= sizeof(type), \
-			pos += sizeof(type), \
-			1 \
-		))
-#define PUSH_UINT8(value) PUSH(value, uint8_t, )
-#define PUSH_UINT16(value) PUSH(value, uint16_t, htons)
-#define PUSH_UINT32(value) PUSH(value, uint32_t, htonl)
+	    ( \
+		type = func(value), \
+		memcpy(pos, &type, sizeof(type)), \
+		length -= sizeof(type), \
+		pos += sizeof(type), \
+		1 \
+	    ))
+#define PUSH_UINT8(value) PUSH(value, types.uint8, )
+#define PUSH_UINT16(value) PUSH(value, types.uint16, htons)
+#define PUSH_UINT32(value) PUSH(value, types.uint32, htonl)
 #define PUSH_BYTES(value, bytes) \
 	((length >= (bytes)) && \
-		( \
-			memcpy(pos, value, (bytes) * sizeof(uint8_t)), \
-			length -= (bytes), \
-			pos += (bytes), \
-			1 \
-		))
+	    ( \
+		memcpy(pos, value, (bytes) * sizeof(uint8_t)), \
+		length -= (bytes), \
+		pos += (bytes), \
+		1 \
+	    ))
 
 #define START_CDP_TLV(type) \
 	( \
-		tlv = pos, \
-		PUSH_UINT16(type) && PUSH_UINT16(0) \
+	    tlv = pos, \
+	    PUSH_UINT16(type) && PUSH_UINT16(0) \
 	)
 #define END_CDP_TLV \
 	( \
-		*((uint16_t *)tlv + 1) = htons(pos - tlv) \
+	    types.uint16 = htons(pos - tlv), \
+	    memcpy((uint16_t *)tlv + 1, &types.uint16, sizeof(uint16_t)) \
 	)
 
 #define START_LLDP_TLV(type) \
 	( \
-		tlv = pos, \
-		PUSH_UINT16(type << 9) \
+	    tlv = pos, \
+	    PUSH_UINT16(type << 9) \
 	)
 #define END_LLDP_TLV \
 	( \
-	*((uint16_t *)tlv) |= htons((pos - (tlv + 2)) & 0x01ff) \
+	    memcpy(&types.uint16, tlv, sizeof(uint16_t)), \
+	    types.uint16 |= htons((pos - (tlv + 2)) & 0x01ff), \
+	    memcpy(tlv, &types.uint16, sizeof(uint16_t)) \
 	)
 
