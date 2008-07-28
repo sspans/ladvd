ea8f39aa4b4b2691e0b953ea3335b49af8c8dc49
sten <devnull@localhost>
2008-07-28 11:14:37 +0000

    integrate normal, and debian source building into one script

--- scripts/release.sh
+++ scripts/release.sh
@@ -1,4 +1,28 @@
-#!/bin/sh
+#!/bin/bash
+
+BASE=$(pwd)
+RELEASE="${BASE}/release"
+
+# prepare
 svn -q up
-svn log | ./scripts/gnuify-changelog.pl > doc/ChangeLog
+svn log | ${BASE}/scripts/gnuify-changelog.pl > ${BASE}/doc/ChangeLog
 autoreconf -fi
+
+# create release dir
+[ -d ${RELEASE} ] && rm -rf ${RELEASE}
+mkdir -p ${RELEASE}/debian
+
+# create dist tarball
+./configure 
+make distcheck
+mv *tar.gz ${RELEASE}
+
+# create debian sources
+cd ${RELEASE}/debian
+tar xf ../*tar.gz
+cd ladvd-*
+dpkg-buildpackage -S
+cd ${RELEASE}/debian
+rm -rf ladvd-*
+
+cd ${BASE}
