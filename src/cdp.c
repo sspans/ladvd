8ef9a73981cf0e9a477d4f3f529aa47839bbc53a
sten <devnull@localhost>
2008-09-14 20:30:16 +0000

    use cap_active for cdp capabilities as requested by Alexander Ottl

--- src/cdp.c
+++ src/cdp.c
@@ -112,12 +112,12 @@ int cdp_packet(struct packet *packet, struct netif *netif,
 
 
     // capabilities
-    if (sysinfo->cap == CAP_HOST) {
+    if (sysinfo->cap_active == CAP_HOST) {
 	cap = CDP_CAP_HOST;
     } else {
-	cap |= (sysinfo->cap & CAP_BRIDGE) ? CDP_CAP_TRANSPARENT_BRIDGE : 0;
-	cap |= (sysinfo->cap & CAP_ROUTER) ? CDP_CAP_ROUTER : 0;
-	cap |= (sysinfo->cap & CAP_SWITCH) ? CDP_CAP_SWITCH : 0;
+	cap |= (sysinfo->cap_active & CAP_BRIDGE) ? CDP_CAP_TRANSPARENT_BRIDGE : 0;
+	cap |= (sysinfo->cap_active & CAP_ROUTER) ? CDP_CAP_ROUTER : 0;
+	cap |= (sysinfo->cap_active & CAP_SWITCH) ? CDP_CAP_SWITCH : 0;
     }
 
     if (!(
