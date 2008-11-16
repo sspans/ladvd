4dea7710b2e857261e06591ba44933e5c4f13942
sten <devnull@localhost>
2008-11-16 17:30:17 +0000

    move the pcap structs to master.h

--- src/master.h
+++ src/master.h
@@ -23,4 +23,24 @@ void master_init(struct passwd *pwd, int cmdfd);
 int master_rsocket();
 size_t master_rsend(int s, struct master_request *mreq);
 
+
+#define PCAP_MAGIC	0xA1B2C3D4
+
+typedef struct pcap_hdr_s {
+    uint32_t magic_number;   /* magic number */
+    uint16_t version_major;  /* major version number */
+    uint16_t version_minor;  /* minor version number */
+    int32_t  thiszone;       /* GMT to local correction */
+    uint32_t sigfigs;        /* accuracy of timestamps */
+    uint32_t snaplen;        /* max length of captured packets, in octets */
+    uint32_t network;        /* data link type */
+} __attribute__ ((__packed__)) pcap_hdr_t;
+
+typedef struct pcaprec_hdr_s {
+    uint32_t ts_sec;         /* timestamp seconds */
+    uint32_t ts_usec;        /* timestamp microseconds */
+    uint32_t incl_len;       /* number of octets of packet saved in file */
+    uint32_t orig_len;       /* actual length of packet */
+} __attribute__ ((__packed__)) pcaprec_hdr_t;
+
 #endif /* _master_h */
