1a1e9f51bf9ac45d9d55781d2d6f3e91d21ed2cb
sten <devnull@localhost>
2008-11-20 22:45:11 +0000

    add socket/bpf filtering to recv sockets

--- src/master.c
+++ src/master.c
@@ -20,6 +20,9 @@
 #ifdef HAVE_NETPACKET_PACKET_H
 #include <netpacket/packet.h>
 #endif /* HAVE_NETPACKET_PACKET_H */
+#ifdef HAVE_LINUX_FILTER_H
+#include <linux/filter.h>
+#endif /* HAVE_LINUX_FILTER_H */
 #ifdef HAVE_NET_BPF_H
 #include <net/bpf.h>
 #endif /* HAVE_NET_BPF_H */
@@ -37,6 +40,93 @@
 #include "edp.h"
 #include "fdp.h"
 #include "ndp.h"
+
+#ifdef HAVE_NET_BPF_H
+struct bpf_insn master_filter[] = {
+#elif HAVE_LINUX_FILTER_H
+struct sock_filter master_filter[] = {
+#endif
+    // lldp
+    // ether 01:80:c2:00:00:0e
+    BPF_STMT(BPF_LD+BPF_W+BPF_ABS, 0),
+    BPF_JUMP(BPF_JMP+BPF_JEQ+BPF_K, 0x0180C200, 0, 5),
+    BPF_STMT(BPF_LD+BPF_H+BPF_ABS, 4),
+    BPF_JUMP(BPF_JMP+BPF_JEQ+BPF_K, 0x0000000E, 0, 3),
+    // ether proto 
+    BPF_STMT(BPF_LD+BPF_H+BPF_ABS, ETHER_ADDR_LEN * 2),
+    BPF_JUMP(BPF_JMP+BPF_JEQ+BPF_K, ETHERTYPE_LLDP, 0, 1),
+    // accept
+    BPF_STMT(BPF_RET+BPF_K, (u_int)-1),
+
+    // llc dsap & ssap
+    BPF_STMT(BPF_LD+BPF_H+BPF_ABS, ETHER_HDR_LEN),
+    BPF_JUMP(BPF_JMP+BPF_JEQ+BPF_K, 0xAAAA, 1, 0),
+    // reject
+    BPF_STMT(BPF_RET+BPF_K, 0),
+
+    // cdp
+    // ether dst 01:00:0c:cc:cc:cc
+    BPF_STMT(BPF_LD+BPF_W+BPF_ABS, 0),
+    BPF_JUMP(BPF_JMP+BPF_JEQ+BPF_K, 0x01000CCC, 0, 7),
+    BPF_STMT(BPF_LD+BPF_H+BPF_ABS, 4),
+    BPF_JUMP(BPF_JMP+BPF_JEQ+BPF_K, 0x0000CCCC, 0, 5),
+    // llc control + org
+    BPF_STMT(BPF_LD+BPF_W+BPF_ABS, ETH_LLC_CONTROL),
+    BPF_JUMP(BPF_JMP+BPF_JEQ+BPF_K, 0x0300000C, 0, 3),
+    // llc protoid
+    BPF_STMT(BPF_LD+BPF_H+BPF_ABS, ETH_LLC_PROTOID),
+    BPF_JUMP(BPF_JMP+BPF_JEQ+BPF_K, LLC_PID_CDP, 0, 1),
+    // accept
+    BPF_STMT(BPF_RET+BPF_K, (u_int)-1),
+
+    // edp
+    // ether dst 00:0e:2b:cc:cc:cc
+    BPF_STMT(BPF_LD+BPF_W+BPF_ABS, 0),
+    BPF_JUMP(BPF_JMP+BPF_JEQ+BPF_K, 0x000E2B00, 0, 7),
+    BPF_STMT(BPF_LD+BPF_H+BPF_ABS, 4),
+    BPF_JUMP(BPF_JMP+BPF_JEQ+BPF_K, 0x00000000, 0, 5),
+    // llc control + org
+    BPF_STMT(BPF_LD+BPF_W+BPF_ABS, ETH_LLC_CONTROL),
+    BPF_JUMP(BPF_JMP+BPF_JEQ+BPF_K, 0x03000E2B, 0, 3),
+    // llc protoid
+    BPF_STMT(BPF_LD+BPF_H+BPF_ABS, ETH_LLC_PROTOID),
+    BPF_JUMP(BPF_JMP+BPF_JEQ+BPF_K, LLC_PID_EDP, 0, 1),
+    // accept
+    BPF_STMT(BPF_RET+BPF_K, (u_int)-1),
+
+    // fdp
+    // ether dst 01:e0:52:cc:cc:cc
+    BPF_STMT(BPF_LD+BPF_W+BPF_ABS, 0),
+    BPF_JUMP(BPF_JMP+BPF_JEQ+BPF_K, 0x01E052CC, 0, 7),
+    BPF_STMT(BPF_LD+BPF_H+BPF_ABS, 4),
+    BPF_JUMP(BPF_JMP+BPF_JEQ+BPF_K, 0x0000CCCC, 0, 5),
+    // llc control + org
+    BPF_STMT(BPF_LD+BPF_W+BPF_ABS, ETH_LLC_CONTROL),
+    BPF_JUMP(BPF_JMP+BPF_JEQ+BPF_K, 0x0300E052, 0, 3),
+    // llc protoid
+    BPF_STMT(BPF_LD+BPF_H+BPF_ABS, ETH_LLC_PROTOID),
+    BPF_JUMP(BPF_JMP+BPF_JEQ+BPF_K, LLC_PID_FDP, 0, 1),
+    // accept
+    BPF_STMT(BPF_RET+BPF_K, (u_int)-1),
+
+    // ndp
+    // ether dst 01:00:81:00:01:00
+    BPF_STMT(BPF_LD+BPF_W+BPF_ABS, 0),
+    BPF_JUMP(BPF_JMP+BPF_JEQ+BPF_K, 0x01008100, 0, 7),
+    BPF_STMT(BPF_LD+BPF_H+BPF_ABS, 4),
+    BPF_JUMP(BPF_JMP+BPF_JEQ+BPF_K, 0x00000100, 0, 5),
+    // llc control + org
+    BPF_STMT(BPF_LD+BPF_W+BPF_ABS, ETH_LLC_CONTROL),
+    BPF_JUMP(BPF_JMP+BPF_JEQ+BPF_K, 0x03000081, 0, 3),
+    // llc protoid
+    BPF_STMT(BPF_LD+BPF_H+BPF_ABS, ETH_LLC_PROTOID),
+    BPF_JUMP(BPF_JMP+BPF_JEQ+BPF_K, LLC_PID_NDP_HELLO, 0, 1),
+    // accept
+    BPF_STMT(BPF_RET+BPF_K, (u_int)-1),
+
+    // reject
+    BPF_STMT(BPF_RET+BPF_K, 0)
+};
 
 extern unsigned int do_debug;
 extern unsigned int do_recv;
@@ -95,7 +185,6 @@ void master_init(struct netif *netifs, uint16_t netifc, int ac,
 		strlcpy(rfds[i].name, subif->name, IFNAMSIZ);
 		rfds[i].fd = master_rsocket(&rfds[i]);
 
-		//master_rfilter(rfds[i]);
 		i++;
 	    }
 
@@ -309,6 +398,7 @@ int master_rsocket(struct master_rfd *rfd) {
     if (rfd == NULL)
 	return(socket);
 
+    // bind the socket to rfd
     struct sockaddr_ll sa;
     memset(&sa, 0, sizeof (sa));
 
@@ -318,6 +408,20 @@ int master_rsocket(struct master_rfd *rfd) {
 
     if (bind(socket, (struct sockaddr *)&sa, sizeof (sa)) != 0)
 	my_fatal("failed to bind socket to %s", rfd->name);
+
+#ifdef HAVE_LINUX_FILTER_H
+    // install socket filter
+    struct sock_fprog fprog;
+
+    memset(&fprog, 0, sizeof(fprog));
+    fprog.filter = master_filter; 
+    fprog.len = sizeof(master_filter) / sizeof(struct sock_filter);
+
+    if (setsockopt(socket, SOL_SOCKET, SO_ATTACH_FILTER,
+		   &fprog, sizeof(fprog)) < 0)
+	my_fatal("unable to configure socket filter for %s", rfd->name);
+#endif /* HAVE_LINUX_FILTER_H */
+
 #elif HAVE_NET_BPF_H
     int n = 0;
     char *dev;
@@ -332,6 +436,7 @@ int master_rsocket(struct master_rfd *rfd) {
     if (rfd == NULL)
 	return(socket);
 
+    // bind the socket to rfd
     struct ifreq ifr;
 
     // prepare ifr struct
@@ -339,7 +444,17 @@ int master_rsocket(struct master_rfd *rfd) {
     strlcpy(ifr.ifr_name, rfd->name, IFNAMSIZ);
 
     if (ioctl(socket, BIOCSETIF, (caddr_t)&ifr) < 0) {
-	fatal("ioctl failed: %s", strerror(errno));
+	my_fatal("failed to bind socket to %s", rfd->name);
+
+    // install bpf filter
+    struct bpf_program fprog;
+
+    memset(&fprog, 0, sizeof(fprog));
+    fprog.bf_insns = &master_filter; 
+    fprog.bf_len = sizeof(master_filter) / sizeof(struct sock_filter);
+
+    if (ioctl(socket, BIOCSETF, (caddr_t)&prog) < 0) {
+	my_fatal("unable to configure bpf filter for %s", rfd->name);
 #endif
 
     return(socket);
