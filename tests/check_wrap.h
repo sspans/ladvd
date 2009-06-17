dec772b9b477a24e98dddbef874998249c626e48
sten <devnull@localhost>
2009-06-17 09:00:28 +0000

    add headerfile

---
+++ tests/check_wrap.h
@@ -0,0 +1,25 @@
+
+#include <setjmp.h>
+
+#define FAIL_MALLOC	(1 << 0)
+#define FAIL_CALLOC	(1 << 1)
+#define FAIL_STRDUP	(1 << 2)
+#define FAIL_SETGID	(1 << 3)
+#define FAIL_SETUID	(1 << 4)
+#define FAIL_SETGRP	(1 << 5)
+#define FAIL_CHDIR	(1 << 6)
+#define FAIL_CHROOT	(1 << 7)
+#define FAKE_SETGID	(1 << 16)
+#define FAKE_SETUID	(1 << 17)
+#define FAKE_SETGRP	(1 << 18)
+#define FAKE_CHDIR	(1 << 19)
+#define FAKE_CHROOT	(1 << 20)
+#define FAIL_EXIT	(1 << 31)
+
+#define WRAP_FATAL_START() \
+    if (!setjmp(check_wrap_env)) { \
+	check_wrap_opt |= FAIL_EXIT;
+#define WRAP_FATAL_END() \
+    } \
+    check_wrap_opt &= ~FAIL_EXIT;
+
