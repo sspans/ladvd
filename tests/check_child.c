087e0bfe6a1ce6303700caf764ce3b200fd00149
sten <devnull@localhost>
2009-09-06 15:25:04 +0000

    cover more of child_queue

--- tests/check_child.c
+++ tests/check_child.c
@@ -4,7 +4,8 @@
 #include <string.h>
 #include <check.h>
 #include <sys/param.h>
-#include <signal.h>
+#include <sys/stat.h>
+#include <fcntl.h>
 
 #include "common.h"
 #include "util.h"
@@ -18,6 +19,29 @@ uint32_t options = OPT_DAEMON | OPT_CHECK;
 extern struct nhead netifs;
 extern struct mhead mqueue;
 extern struct sysinfo sysinfo;
+
+void read_packet(struct master_msg *msg, const char *suffix) {
+    int fd;
+    char *prefix, *path = NULL;
+
+    memset(msg->msg, 0, ETHER_MAX_LEN);
+    msg->len = 0;
+    msg->ttl = 0;
+    memset(msg->peer.name, 0, IFDESCRSIZE);
+    memset(msg->peer.port, 0, IFDESCRSIZE);
+
+    if ((prefix = getenv("srcdir")) == NULL)
+	prefix = ".";
+
+    fail_if(asprintf(&path, "%s/%s", prefix, suffix) == -1,
+	    "asprintf failed");
+
+    mark_point();
+    fail_if((fd = open(path, O_RDONLY)) == -1, "failed to open %s", path);
+    msg->len = read(fd, msg->msg, ETHER_MAX_LEN);
+
+    free(path);
+}
 
 START_TEST(test_child_send) {
 }
@@ -37,6 +61,7 @@ START_TEST(test_child_queue) {
     memset(&msg, 0, sizeof(struct master_msg));
     msg.cmd = MASTER_RECV;
     msg.len = ETHER_MIN_LEN;
+    msg.proto = PROTO_LLDP;
 
     // unknown interface
     mark_point();
@@ -70,6 +95,34 @@ START_TEST(test_child_queue) {
     fail_unless (strncmp(check_wrap_errstr, errstr, strlen(errstr)) == 0,
 	"incorrect message logged: %s", check_wrap_errstr);
 
+    // valid message contents
+    mark_point();
+    read_packet(&msg, "proto/lldp/42.good.big");
+    write(spair[0], &msg, MASTER_MSG_SIZE);
+    child_queue(spair[1], event);
+
+    // and the same peer again
+    mark_point();
+    read_packet(&msg, "proto/lldp/42.good.big");
+    write(spair[0], &msg, MASTER_MSG_SIZE);
+    child_queue(spair[1], event);
+
+    // test with OPT_AUTO
+    mark_point();
+    options |= OPT_AUTO;
+    read_packet(&msg, "proto/lldp/43.good.lldpmed");
+    write(spair[0], &msg, MASTER_MSG_SIZE);
+    child_queue(spair[1], event);
+
+    // test with OPT_ARGV
+    mark_point();
+    options |= OPT_ARGV;
+    msg.proto = PROTO_CDP;
+    read_packet(&msg, "proto/cdp/45.good.6504");
+    write(spair[0], &msg, MASTER_MSG_SIZE);
+    child_queue(spair[1], event);
+
+    options = OPT_DAEMON | OPT_CHECK;
     TAILQ_REMOVE(&netifs, &netif, entries);
 }
 END_TEST
