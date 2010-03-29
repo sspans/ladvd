2becc2b0b980de83a1240e5243504c93ea515303
sten <devnull@localhost>
2010-03-29 21:40:30 +0000

    remove addr decoding for now, add cap to string

--- src/proto/tlv.c
+++ src/proto/tlv.c
@@ -21,10 +21,13 @@
 #include "util.h"
 #include "proto/tlv.h"
 
+
 void tlv_value_str(struct master_msg *msg,
 	    uint16_t type, uint16_t length, void *value) {
     char src[TLV_LEN], *str = NULL;
-    size_t srclen, strlen;
+    size_t srclen, len;
+    uint16_t cap, i, j = 0;
+    const char *cap_str = CAP_STRING;
 
     // skip if not wanted or already decoded
     if (!(msg->decode & (1 << type)) || msg->peer[type])
@@ -33,36 +36,19 @@ void tlv_value_str(struct master_msg *msg,
     switch (type) {
 	case PEER_HOSTNAME:
 	case PEER_PORTNAME:
-	case PEER_HARDWARE:
-	case PEER_SOFTWARE:
 	    srclen = MIN(length, TLV_LEN - 1);
 	    memcpy(src, value, srclen);
 	    *(src + srclen) = '\0';
-	    strlen = srclen * 4 + 1;
-	    str = my_malloc(strlen);
-	    strnvis(str, src, strlen, VIS_NL|VIS_TAB|VIS_OCTAL);
+	    len = srclen * 4 + 1;
+	    str = my_malloc(len);
+	    strnvis(str, src, len, VIS_NL|VIS_TAB|VIS_GLOB|VIS_OCTAL);
 	    break;
-	case PEER_ETHER:
-	    if (length != sizeof(struct ether_addr))
-		break;
-	    str = ether_ntoa(value);
-	    break;
-	case PEER_IPV4:
-	    if (length != sizeof(struct in_addr))
-		break;
-	    str = my_malloc(INET_ADDRSTRLEN);
-	    if (!inet_ntop(AF_INET, value, str, INET_ADDRSTRLEN)) {
-		free(str);
-		str = NULL;
-	    }
-	    break;
-	case PEER_IPV6:
-	    if (length != sizeof(struct in6_addr))
-		break;
-	    str = my_malloc(INET6_ADDRSTRLEN);
-	    if (!inet_ntop(AF_INET6, value, str, INET6_ADDRSTRLEN)) {
-		free(str);
-		str = NULL;
+	case PEER_CAP:
+	    memcpy(&cap, value, sizeof(uint16_t));
+	    str = my_malloc(CAP_MAX + 1);
+	    for (i = 0; i < CAP_MAX; i++) {
+		if (cap & (1 << i))
+		    str[j++] = cap_str[i];
 	    }
 	    break;
 	default:
