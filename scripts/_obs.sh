022d75569fb9974c396d03e9909a44d4377336dc
Sten Spans <sten@blinkenlights.nl>
2010-09-15 16:18:56 +0200

    rename ladvd-devel to ladvd-unstable

--- scripts/_obs.sh
+++ scripts/_obs.sh
@@ -8,9 +8,10 @@ cd ${RELEASE}/osc.$$
 osc checkout home:sten-blinkenlights
 mv home\:sten-blinkenlights ${RELEASE}/osc
 rm -rf ${RELEASE}/osc.$$
-cp ${RELEASE}/*.tar.gz ${RELEASE}/osc/${NAME}-devel
-cp ${BASE}/rpm/* ${RELEASE}/osc/${NAME}-devel
-[ -d ${RELEASE}/debian ] && cp ${RELEASE}/debian/* ${RELEASE}/osc/${NAME}-devel
+cp ${RELEASE}/*.tar.gz ${RELEASE}/osc/${NAME}-unstable
+cp ${BASE}/rpm/* ${RELEASE}/osc/${NAME}-unstable
+[ -d ${RELEASE}/debian ] && \
+    cp ${RELEASE}/debian/* ${RELEASE}/osc/${NAME}-unstable
 
 cd ${BASE}
 
