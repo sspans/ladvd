04b1c6918d2ec64d35bcf9430e551fd3bae45ec2
Sten Spans <sten@blinkenlights.nl>
2014-11-04 16:26:32 +0100

    test for tlv_value_str

--- tests/check_tlv.c
+++ tests/check_tlv.c
@@ -30,6 +30,63 @@
 
 uint32_t options = OPT_DAEMON | OPT_CHECK;
 
+START_TEST(test_value_str) {
+    struct master_msg msg = {};
+    uint16_t type = 0;
+    uint16_t length = 0;
+    char value[32] = {};
+    const char *errstr = NULL;
+
+    mark_point();
+    type = PEER_ADDR_INET4;
+    length = 0;
+    tlv_value_str(&msg, type, length, value);
+    fail_unless (msg.peer[type] == NULL, "a NULL pointer should be returned");
+    length = 5;
+    tlv_value_str(&msg, type, length, value);
+    fail_unless (msg.peer[type] == NULL, "a NULL pointer should be returned");
+    length = 4;
+    tlv_value_str(&msg, type, length, value);
+    fail_unless (msg.peer[type] != NULL, "a valid pointer should be returned");
+
+    mark_point();
+    type = PEER_ADDR_INET6;
+    length = 0;
+    tlv_value_str(&msg, type, length, value);
+    fail_unless (msg.peer[type] == NULL, "a NULL pointer should be returned");
+    length = 17;
+    tlv_value_str(&msg, type, length, value);
+    fail_unless (msg.peer[type] == NULL, "a NULL pointer should be returned");
+    length = 16;
+    tlv_value_str(&msg, type, length, value);
+    fail_unless (msg.peer[type] != NULL, "a valid pointer should be returned");
+
+    mark_point();
+    type = PEER_ADDR_802;
+    length = 0;
+    tlv_value_str(&msg, type, length, value);
+    fail_unless (msg.peer[type] == NULL, "a NULL pointer should be returned");
+    length = 7;
+    tlv_value_str(&msg, type, length, value);
+    fail_unless (msg.peer[type] == NULL, "a NULL pointer should be returned");
+    length = 6;
+    tlv_value_str(&msg, type, length, value);
+    fail_unless (msg.peer[type] != NULL, "a valid pointer should be returned");
+
+    mark_point();
+    type = PEER_MAX + 1;
+    length = 0;
+    errstr = "unhandled type";
+    WRAP_FATAL_START();
+    tlv_value_str(&msg, type, length, value);
+    WRAP_FATAL_END();
+    fail_unless (strncmp(check_wrap_errstr, errstr, strlen(errstr)) == 0,
+	"incorrect message logged: %s", check_wrap_errstr);
+
+    peer_free(msg.peer);
+}
+END_TEST
+
 START_TEST(test_str_addr) {
     char *str = NULL;
     void *pos;
@@ -82,7 +139,8 @@ Suite * tlv_suite (void) {
     Suite *s = suite_create("proto/tlv.c");
 
     // tlv test case
-    TCase *tc_tlv = tcase_create("util");
+    TCase *tc_tlv = tcase_create("tlv");
+    tcase_add_test(tc_tlv, test_value_str);
     tcase_add_test(tc_tlv, test_str_addr);
     suite_add_tcase(s, tc_tlv);
 
