e0de888021244aeb7994343e93131db2cb812f7f
Sten Spans <sten@blinkenlights.nl>
2014-12-08 09:32:11 +0100

    match all pcap errors - for older libpcap

--- tests/check_master.c
+++ tests/check_master.c
@@ -448,7 +448,7 @@ START_TEST(test_master_socket) {
 	return;
 
     options &= ~OPT_DEBUG;
-    errstr = "pcap_activate";
+    errstr = "pcap_";
     WRAP_FATAL_START();
     master_socket(rfd);
     WRAP_FATAL_END();
